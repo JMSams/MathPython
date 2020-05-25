392d1fd2740ae8e0b16d502d92f0661a74ceebac
Jamie <sams_drum@hotmail.com>
2020-05-25 01:53:25 +0100

    .

--- X Prisoner Problem/xprisoners.py
+++ X Prisoner Problem/xprisoners.py
@@ -4,44 +4,87 @@ import threading
 import time
 import sys
 
-simCount = 1000000
+from decimal import Decimal,getcontext
+getcontext().prec = 64
 
-def prisoners(count):
+from os import system,name,path
+if name == 'nt':
+    _ = system('cls')
+else:
+    _ = system('clear')
+
+currentPrisoner = -1
+currentSim = -1
+
+def prisoners():
+    global currentSim, currentPrisoner, prisonerCount
+
     drawers = []
-    for i in range(count):
+    for i in range(prisonerCount):
         drawers.append(i)
     
-    results = 0
-    for sim in range(simCount):
-        #shuffle drawers
-        random.shuffle(drawers)
+    with open(path.join(sys.path[0], 'results for {} prisoners.csv'.format(prisonerCount)), 'w') as f:
+        f.write('sim,win,')
+        f.write(','.join(map(str, drawers)))
+        f.write('\n')
 
-        #check all prisoners
-        successes = 0
-        for prisoner in range(count):
-            if prisoner in random.sample(drawers, round(count/2)):
-                successes += 1
-        
-        if successes == count:
-            results += 1
+        results = 0
+        for sim in range(simCount):
+            currentSim = sim
+
+            f.write(str(sim) + ',')
+
+            #shuffle drawers
+            random.shuffle(drawers)
+
+            #check all prisoners
+            successes = 0
+            for prisoner in range(prisonerCount):
+                currentPrisoner = prisoner
+                #for half the drawers...
+                d = prisoner
+                for i in range(round(prisonerCount/2)):
+                    if drawers[d] == prisoner:
+                        successes += 1
+                        break
+                    else:
+                        d = drawers[d]
+            
+            if successes == prisonerCount:
+                f.write('yes,')
+                results += 1
+            else:
+                f.write('no,')
+            f.write(','.join(map(str, drawers)))
     
     global done
     done = True
-    print("\rPrisoner Count: {}".format(count))
-    print("Simulation Count: {}".format(simCount))
-    print("Success Rate: {}".format(results / simCount))
 
-done = False
+    #with open(path.join(sys.path[0], 'results for {} prisoners.csv'.format(prisonerCount)), 'w') as f:
+    #    f.write("Prisoner Count: {}\n".format(prisonerCount))
+    #    f.write("Simulation Count: {}\n".format(simCount))
+    #    f.write("Success Rate: {}/{} ({}%)\n".format(results, simCount, Decimal(Decimal(results)/Decimal(simCount))*Decimal(100)))
+    #    f.write("-------------\n")
+
 def animate():
-    for c in itertools.cycle(['.', '..', '...']):
+    for c in itertools.cycle(['\\', '|', '/', '-']):
         if done:
+            print("\rSimulations for {} prisoners complete!       ".format(prisonerCount))
             break
-        sys.stdout.write('\working' + c)
+        print("\rWorking ({3} Prisoners) (Sim: {1}/{2}) {0}".format(c, currentSim, simCount, prisonerCount), end="")
         sys.stdout.flush()
-        time.sleep(0.1)
+        time.sleep(0.2)
 
-t = threading.Thread(target=animate)
-t.start()
+def StartAnimator():
+    t = threading.Thread(target=animate)
+    t.daemon = True
+    
+    global done
+    done = False
+    t.start()
 
-prisoners(20)
-done = True
+prisonerCount = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+simCount = int(sys.argv[2]) if len(sys.argv) > 2 else 100000
+
+StartAnimator()
+prisoners()
